7f70863a63cefae5b4aa4289f9b18471843cccad
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-21 22:41:56 -0300

    Update model_contatos.dart

--- lista_contatos/lib/src/model/model_contatos.dart
+++ lista_contatos/lib/src/model/model_contatos.dart
@@ -12,6 +12,31 @@ class ContatosModel {
       required this.telefone,
       required this.email,
       required this.tipo});
+
+  static List<ContatosModel> getModel() {
+    return <ContatosModel>[
+      ContatosModel(
+          nome: 'Thiago',
+          email: 'thiago@gmail.com',
+          telefone: '3443-2558',
+          tipo: ContatoType.casa),
+      ContatosModel(
+          nome: 'Joao',
+          email: 'Joao@gmail.com',
+          telefone: '3443-4529',
+          tipo: ContatoType.trabalho),
+      ContatosModel(
+          nome: 'Victor',
+          email: 'Victor@gmail.com',
+          telefone: '99856-2558',
+          tipo: ContatoType.celular),
+      ContatosModel(
+          nome: 'Andrei',
+          email: 'Andrei@gmail.com',
+          telefone: '3443-2558',
+          tipo: ContatoType.favorito),
+    ];
+  }
 }
 
 enum ContatoType { celular, trabalho, favorito, casa }
