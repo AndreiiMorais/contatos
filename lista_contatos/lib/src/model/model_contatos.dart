f745191badb76b667c5ef428432e05b5d2a12aab
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-21 22:35:25 -0300

    created model

--- lista_contatos/lib/src/model/model_contatos.dart
+++ lista_contatos/lib/src/model/model_contatos.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
-class ContatosModel{
+
+class ContatosModel {
   final String nome;
   final String telefone;
   final String email;
-  final tipo;
+  final ContatoType tipo;
+  final bool check = false;
+
+  ContatosModel(
+      {required this.nome,
+      required this.telefone,
+      required this.email,
+      required this.tipo});
 }
+
+enum ContatoType { celular, trabalho, favorito, casa }
