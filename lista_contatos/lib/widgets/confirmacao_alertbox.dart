0c263a9e5df5ba1ba718f69eaa7e0a2cbdbcf8a9
Andrei Silveira Morais <andrei.morais@outlook.com>
2022-01-06 09:52:11 -0300

    Update confirmacao_alertbox.dart

--- lista_contatos/lib/widgets/confirmacao_alertbox.dart
+++ lista_contatos/lib/widgets/confirmacao_alertbox.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:lista_contatos/src/controle/db_Control.dart';
+import 'package:lista_contatos/src/settings.dart';
 
 class ConfirmacaoAlerBox extends StatelessWidget {
-  const ConfirmacaoAlerBox({ Key? key }) : super(key: key);
+  DbControl control = DbControl();
+  const ConfirmacaoAlerBox({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return AlertDialog(
+      title: const Text('Editar Contato'),
+      content: const Text('Você tem certeza?'),
+      actions: [
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Excluir'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Cancelar'),
+        )
+      ],
     );
   }
 }
+
+
+// AlertDialog(
+//       title: const Text('Editar Contato'),
+//       content: Column(
+//         children: [
+//           TextField(
+//             textCapitalization: TextCapitalization.words,
+//             controller: name,
+//             autofocus: true,
+//             decoration: const InputDecoration(
+//               labelText: 'Nome',
+//             ),
+//           ),
+//           TextField(
+//             controller: phone,
+//             autofocus: true,
+//             decoration: const InputDecoration(
+//               labelText: 'Telefone',
+//             ),
+//           ),
+//           TextField(
+//             controller: email,
+//             autofocus: true,
+//             decoration: const InputDecoration(
+//               labelText: 'Email',
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
